5f42391503850dc5a6ffeeae43345c6fe822a8fd
Zanardi <63323990+brunozanardi@users.noreply.github.com>
2021-10-04 18:40:27 -0300

    Update Projeto_Bancada.ino

--- Projeto_Bancada.ino
+++ Projeto_Bancada.ino
@@ -10,7 +10,7 @@
 const int pitot_tube_pin = A0; //PINO UTILIZADO PELO TUBO DE PITOT
 const int Sensor_tensao_controle = A1; //PINO UTILIZADO PELO SENSOR DE TENSÃO DO SISTEMA DE CONTROLE
 const int Sensor_tensao_potencia = A2;//PINO UTILIZADO PELO SENSOR DE TENSÃO DO SISTEMA DE POTENCIA
-const int Sensor_corrente_controle = A3; //PINO UTILIZADO PELO SENSOR DE CORRENTE DO SISTEMA DE CONTROLE
+const int sensor_corrente_cont = A3; //PINO UTILIZADO PELO SENSOR DE CORRENTE DO SISTEMA DE CONTROLE
 const int c_sens = A4; //PINO UTILIZADO PELO SENSOR DE CORRENTE DO SISTEMA DE POTENCIA
 const int LM35 = A5; //PINO UTILIZADO PELO SENSOR DE TEMPERATURA LM35
 
@@ -68,10 +68,11 @@ HX711_ADC LoadCell(HX711_dout, HX711_sck);//Amplificador HX711
 const int calVal_eepromAdress = 0;
 long t;
 float peso;
+float i;
 
 //PARÂMETROS PARA O SENSOR DE TEMPERATURA LM35
-float temperatura; // Variável que armazenará a temperatura média medida
-float voltage_lm35; // Variável que coleta a saída de tensão do LM35
+
+float temperatura; // Variável que armazenará a temperatura medida
 
 //VARIÁVEIS CRIADAS PARA A SAÍDA DA PORTA SERIAL
 
@@ -96,27 +97,27 @@ float temperatura_bateria_function();
 
 void setup() {
   //DEFINIÇÃO DO MODO DE CADA UM DOS PINOS DE INPUT
-  pinMode(2, INPUT_PULLUP);  
-  pinMode(c_sens, INPUT_PULLUP);
-  pinMode(Sensor_tensao_controle, INPUT_PULLUP); 
-  pinMode(Sensor_tensao_potencia, INPUT_PULLUP);
-  pinMode(Sensor_corrente_controle, INPUT_PULLUP);
-  pinMode(LM35, INPUT_PULLUP); //valor de pull-up médio 480
-  pinMode(pitot_tube_pin, INPUT_PULLUP);
+  pinMode(2, INPUT);  
+  //pinMode(c_sens, INPUT);
+  //pinMode(Sensor_tensao_controle, INPUT); 
+  //pinMode(Sensor_tensao_potencia, INPUT);
+  //pinMode(sensor_corrente_cont, INPUT);
+  //pinMode(LM35, INPUT); //valor de pull-up médio 480
+  pinMode(pitot_tube_pin, INPUT);
   //PORTA SERIAL DEFINIDA COMO 9600 BPS
   Serial.begin(9600);
-  delay(10);
+  //delay(10);
 
   //SETUP PARA A CÉLULA DE CARGA
   LoadCell.begin();
   float calibrationValue; // calibration value (see example file "Calibration.ino")
-  calibrationValue = -450.15; // uncomment this if you want to set the calibration value in the sketch
-#if defined(ESP8266)|| defined(ESP32)
+  calibrationValue = -104.94; // uncomment this if you want to set the calibration value in the sketch
+  #if defined(ESP8266)|| defined(ESP32)
   //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
-#endif
+  #endif
   //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
 
-  long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  long stabilizingtime = 0; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
   boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
   LoadCell.start(stabilizingtime, _tare);
   if (LoadCell.getTareTimeoutFlag()) {
@@ -127,52 +128,72 @@ void setup() {
     LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
     //Serial.println("Startup is complete");
   }
+ 
 }
 
   
 void loop() {
 
   //PUXANDO VALORES DAS FUNÇÕES E COLOCANDO EM VARIÁVEIS
-  
+  tensao_bateria_pot = tensao_bateria_potencia(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE TENSÃO  
   massa = Load_Cell(); //EXTRAÇÃO DO VALOR DE SAÍDA DA FUNÇÃO DA CÉLULA DE CARGA
-  rotacao = RPM_3144E(); //EXTRAÇÃO DO VALOR DE SAÍDA DA FUNÇÃO DE RPM
+  rotacao = (RPM_3144E()/2); //EXTRAÇÃO DO VALOR DE SAÍDA DA FUNÇÃO DE RPM
   velocidade = pitot(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR TUBO DE PITOT
   tensao_bateria_control = tensao_bateria_controle(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE TENSÃO
-  tensao_bateria_pot = tensao_bateria_potencia(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE TENSÃO
-  corrente_bateria_control = sensor_corrente_controle(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE CORRENTE
   temperatura_bateria = temperatura_bateria_function(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE TENSÃO
+  corrente_bateria_control = sensor_corrente_controle(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE CORRENTE
   corrente_bateria_potencia = sensor_corrente_potencia(); //EXTRAÇÃO DO VALOR DE SAÍDA DO SENSOR DE TEMPERATURA
 
   //COLOCANDO VARIÁVEIS NA PORTA SERIAL
   
-//    Serial.print(massa); //valor quando acionado o pull-up = -3689.77
-//    Serial.print(0);
-//    Serial.print(",");
-//    Serial.print(rotacao);//valor quando acionado o pull-up < 0
-//    Serial.print(0);
-//    Serial.print(",");
-//    Serial.print(velocidade);
-//    Serial.print(0);
-//    Serial.print(",");
-//    Serial.print(tensao_bateria_control); //valor quando acionado o pull-up = 23.63
-//    Serial.print(10);
-//    Serial.print(",");
-//    Serial.print(tensao_bateria_pot); //valor quando acionado o pull-up = 21.9
-//    Serial.print(18);
-//    Serial.print(",");
-//    Serial.print(corrente_bateria_control); //valor quando acionado o pull-up = 36.4
-//    Serial.print(0);
-//    Serial.print(",");
-//    Serial.print(temperatura_bateria); //valor quando acionado o pull-up = 485
-//    Serial.print(25);
-//    Serial.print(",");
-
-//    Serial.print(corrente_bateria_potencia); //valor quando acionado o pull-up = -59.36
+    Serial.print(massa); //valor quando acionado o pull-up = -3689.77
+    Serial.print(",");
+    Serial.print(rotacao);//valor quando acionado o pull-up < 0
+    Serial.print(",");
+    Serial.print(velocidade);
+    Serial.print(",");
+    Serial.print(tensao_bateria_control); //valor quando acionado o pull-up = 23.63
+    Serial.print(",");
+    Serial.print(tensao_bateria_pot); //valor quando acionado o pull-up = 21.9
+    Serial.print(",");
+    Serial.print(corrente_bateria_control); //valor quando acionado o pull-up = 36.4
+    Serial.print(",");
+    Serial.print(temperatura_bateria); //valor quando acionado o pull-up = 485
+    Serial.print(",");
+    Serial.print(corrente_bateria_potencia); //valor quando acionado o pull-up = -59.36
     Serial.print("\n");
-    delay(200);
+    delay(100);
 }
-float Load_Cell(){
 
+float tensao_bateria_potencia(){ 
+   leituraSensor = analogRead(Sensor_tensao_potencia); //FAZ A LEITURA DO PINO ANALÓGICO E ARMAZENA NA VARIÁVEL O VALOR LIDO
+   tensaoEntrada = (leituraSensor * 5.0) / 1024.0; //VARIÁVEL RECEBE O RESULTADO DO CÁLCULO
+   tensaoMedida = (tensaoEntrada / (valorR2/(valorR1+valorR2)))-offset_pot; //VARIÁVEL RECEBE O VALOR DE TENSÃO DC MEDIDA PELO SENSOR
+   return tensaoMedida;
+
+}
+
+
+float sensor_corrente_potencia(){
+
+  QOV = quiescent_Output_voltage*5.0;
+  cutoff = 0.04/cutOffLimit;
+  volt = (5.0 / 1023.0)*float(analogRead(c_sens));
+  volt = volt - QOV - 0.067;
+  offset_corrente_pot = 0;
+  c_value = (volt/0.04 - (offset_corrente_pot))*-1;
+  return c_value;
+}
+
+float sensor_corrente_controle(){
+  RawValue = float(analogRead(sensor_corrente_cont))-40;
+  Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
+  Amps = ((Voltage - ACSoffset) / mVperAmp);
+  return Amps;
+}
+
+float Load_Cell() {
+//  float i = LoadCell.getData();
   static boolean newDataReady = 0;
   const int serialPrintInterval = 0; //increase value to slow down serial print activity
 
@@ -181,36 +202,23 @@ float Load_Cell(){
 
   // get smoothed value from the dataset:
   if (newDataReady) {
-    if (millis() > t + serialPrintInterval) {
-      float i = LoadCell.getData()*-1;
+    //if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
       //Serial.print("Load_cell output val: ");
       //Serial.println(i);
       newDataReady = 0;
       t = millis();
-      return -i;
-    }
-  }
-
-  // receive command from serial terminal, send 't' to initiate tare operation:
-  if (Serial.available() > 0) {
-    float i;
-    char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay();
-  }
-
-  // check if last tare operation is complete:
-  if (LoadCell.getTareStatus() == true) {
-    Serial.println("Tare complete");
-  }
-
-
+      return i;
+    //}
+}
+  
 }
 
 float RPM_3144E(){
   revolutions=0; rpm=0;
   startTime=millis();         
   attachInterrupt(digitalPinToInterrupt(2),interruptFunction,RISING);
-  delay(1000);
+  delay(10);
   detachInterrupt(2);                
   
   //now let's see how many counts we've had from the hall effect sensor and calc the RPM
@@ -256,35 +264,8 @@ float tensao_bateria_controle(){
    return tensaoMedida;
 }
 
-float tensao_bateria_potencia(){ 
-   leituraSensor = analogRead(Sensor_tensao_potencia); //FAZ A LEITURA DO PINO ANALÓGICO E ARMAZENA NA VARIÁVEL O VALOR LIDO
-   tensaoEntrada = (leituraSensor * 5.0) / 1024.0; //VARIÁVEL RECEBE O RESULTADO DO CÁLCULO
-   tensaoMedida = (tensaoEntrada / (valorR2/(valorR1+valorR2)))-offset_pot; //VARIÁVEL RECEBE O VALOR DE TENSÃO DC MEDIDA PELO SENSOR
-   return tensaoMedida;
-
-}
-
-float sensor_corrente_controle(){
-  RawValue = analogRead(Sensor_corrente_controle);
-  Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
-  Amps = ((Voltage - ACSoffset) / mVperAmp);
-  return Amps;
-}
-
-float sensor_corrente_potencia(){
-  QOV = quiescent_Output_voltage*5.0;
-  cutoff = 0.04/cutOffLimit;
-  volt = (5.0 / 1023.0)*analogRead(c_sens);
-  volt = volt - QOV - 0.067;
-  offset_corrente_pot = 0;
-  c_value = (volt/0.04 - (offset_corrente_pot))*-1;
-  return c_value;
-}
-
-
 float temperatura_bateria_function(){
-
-  voltage_lm35 = (float(analogRead(LM35))*5/1023);
-  temperatura = voltage_lm35/0.01;
+  //temperatura = float(analogRead(LM35));
+  temperatura = (float(analogRead(LM35))*5/(1023))/0.01;
   return temperatura;
 }
